70836647c93576e42ace4becae4ceed5aa2c576e
Aerain <aerain@Aerainui-MacBook-Pro.local>
2018-10-01 15:41:00 +0900

    오류 해결

--- ios/jedaeroReactNative/AppDelegate.m
+++ ios/jedaeroReactNative/AppDelegate.m
@@ -22,7 +22,7 @@
                                                       moduleName:@"jedaeroReactNative"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1 green:1 blue:1 alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
