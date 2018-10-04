4fa55ed9713b16bd68d2c4fd6f24c11a908b73ef
aerain <j40f893@gmail.com>
2018-10-04 14:45:44 +0900

    ㅔㅌ스트

--- activity/drawer/library/LibrarySearch.js
+++ activity/drawer/library/LibrarySearch.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View, Button, TextInput } from 'react-native'
 import { SafeAreaView } from 'react-navigation';
-import { SearchBar } from 'react-native-elements';
+import { SearchBar, normalize } from 'react-native-elements';
 
 export default class LibrarySearch extends Component {
   static navigationOptions= {
@@ -16,10 +16,10 @@ export default class LibrarySearch extends Component {
   }
   render() {
     return (
-      <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center',}}forceInset={{bottom: "always"}}>
+      <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}forceInset={{bottom: "always"}}>
         <TextInput 
         placeholder="책 제목을 입력하세요"
-        style={{width: '100%', paddingHorizontal: 32}}
+        style={{width: '100%', paddingHorizontal: 32, fontSize:normalize(30), fontWeight: '100',}}
         onChangeText={search => this.setState({search})}
         />
         <Button
