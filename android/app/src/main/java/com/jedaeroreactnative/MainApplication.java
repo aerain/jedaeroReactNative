86fd3ea9c3a67fa9bd0ac1e95c9053e01eec3d2b
aerain <j40f893@gmail.com>
2019-03-12 05:09:12 +0900

    upgraded to 0.58.3

--- android/app/src/main/java/com/jedaeroreactnative/MainApplication.java
+++ android/app/src/main/java/com/jedaeroreactnative/MainApplication.java
@@ -3,9 +3,8 @@ package com.jedaeroreactnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.brentvatne.react.ReactVideoPackage;
-import com.localz.PinchPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
+import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,9 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactVideoPackage(),
-            new PinchPackage(),
             new RNFetchBlobPackage(),
+            new ReactVideoPackage(),
             new VectorIconsPackage()
       );
     }
